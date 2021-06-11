25aff25ebf375ff87351181be626e25cc2d16b23
HoangNguyenHuy1220 <1851171561@e.tlu.edu.vn>
2021-06-11 18:17:46 +0700

    Xoa viewpager thay bang navigation component

--- app/src/main/java/com/ltud/fooddelivery/MainActivity.java
+++ app/src/main/java/com/ltud/fooddelivery/MainActivity.java
@@ -17,7 +17,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView mNavigationView;
-    private ViewPager mViewPager;
     private ImageView com,douong,anvat;
 
     @Override
@@ -26,71 +25,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mNavigationView = findViewById(R.id.bottom_nav);
-        mViewPager = findViewById(R.id.view_pager);
 
-        setUpViewPager();
 
-        mNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.action_home:
-                        mViewPager.setCurrentItem(0);
-                        break;
-                    case R.id.action_order:
-                        mViewPager.setCurrentItem(1);
-                        break;
-                    case R.id.action_noti:
-                        mViewPager.setCurrentItem(2);
-                        break;
-                    case R.id.action_fav:
-                        mViewPager.setCurrentItem(3);
-                        break;
-                    case R.id.action_user:
-                        mViewPager.setCurrentItem(4);
-                        break;
-                }
-                return true;
-            }
-        });
 
-    }
-
-    private void setUpViewPager(){
-        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        mViewPager.setAdapter(viewPagerAdapter);
-
-        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                switch (position) {
-                    case 0:
-                        mNavigationView.getMenu().findItem(R.id.action_home).setChecked(true);
-                        break;
-                    case 1:
-                        mNavigationView.getMenu().findItem(R.id.action_order).setChecked(true);
-                        break;
-                    case 2:
-                        mNavigationView.getMenu().findItem(R.id.action_noti).setChecked(true);
-                        break;
-                    case 3:
-                        mNavigationView.getMenu().findItem(R.id.action_fav).setChecked(true);
-                        break;
-                    case 4:
-                        mNavigationView.getMenu().findItem(R.id.action_user).setChecked(true);
-                        break;
-                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
     }
 }
